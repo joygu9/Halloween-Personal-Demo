64a2777967d56f9c143114877049efe5310ad6d4
Joy Gu <jgu9@berkeley.edu>
2015-10-10 17:46:37 -0700

    Finished Personal Demo

---
+++ Assets/Scripts/StackBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StackBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > 1.4) {
+			Vector3 player = OVRManager.instance.transform.position;
+			if (player.x > 4 && player.x < 6) {
+				if (player.z < 10) {
+					double x = transform.position.x;
+					double z = transform.position.z;
+					transform.position = new Vector3((float) x, (float) 1.4, (float) z);
+					GetComponent<AudioSource>().Play();
+				}
+			}
+		} 	
+	}
+
+}
---
+++ Assets/Scripts/VoldyBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoldyBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 player = OVRManager.instance.transform.position;
+		if (player.x > 4 && player.x < 6) {
+			if (player.z < 8) {
+				double x = transform.position.x;
+				double y = transform.position.y;
+				double z = transform.position.z;
+					
+				if (z <=8){
+					transform.position = new Vector3((float) x, (float) y, (float) (7));
+					GetComponent<AudioSource>().Play();
+				}
+			}
+		} 	
+	}
+}
